48b08ea6576493a6a987781302dd60a57e1eec64
alexhrinyov <hrinyou29@gmail.com>
2023-03-02 21:59:11 +0300

    Add project files.

---
+++ BDApp/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDApp
+{
+    public static class ConnectionString
+    {
+        public static string MsSqlConnection => @"Server = DESKTOP-BQ6LNRN\SQLEXPRESS; TrustServerCertificate = true; Database = testing; Trusted_Connection=True";
+    }
+}
---
+++ BDApp/MainConnector.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace BDApp
+{
+    public class MainConnector
+    {
+        private SqlConnection connection;
+        public async Task<bool> ConnectAsync()
+        {
+            bool result;
+            try
+            {
+                connection = new SqlConnection(ConnectionString.MsSqlConnection);
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+
+        public async void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                await connection.CloseAsync();
+            }
+        }
+
+    }
+}
